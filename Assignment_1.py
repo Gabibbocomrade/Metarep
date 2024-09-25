9d86048d54437348fca3e8d65286dbdf518a4b1b
Gabibbocomrade <Tank2001@hotmail.it>
2024-09-25 15:44:15 +0200

    Modified Code

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,34 +1,40 @@
-from loguru import logger
+
 import argparse
 import string
 
+from loguru import logger
 file_path = "pg1497.txt"
 
-def count_characters(file_path):
+def count_characters(file_path, plot_histogram):
     counts = {char: 0 for char in string.ascii_lowercase}
     #Rough equivalent
     # counts = {}
     # for char in string_ascii_lowercase
     #   counts{char} = 0
-    with open(file_path, encoding="utf8") as input_file:
+    with open(file_path) as input_file:
         logger.debug(f"Reading input data from {file_path}...")
         data = input_file.read()
     logger.debug(f"Done, {len(data)} character(s) found.")
     logger.info("Counting characters")
-    for char in data:
+    for char in data.lower():
         if char in counts:   
             counts[char] += 1
     logger.info(f"Character counts: {counts}")
     num_characters = sum(counts.values())
-    logger.info(f"Total nnumber of characters: {num_characters}")
+    logger.info(f"Total number of characters: {num_characters}")
     for key, value in counts.item():
-        counts(key) == value / num_characters
+        counts[key] = value / num_characters
     logger.info(f"Character frequencies: {counts}")
+    if plot_histogram:
+        # Do something...
+        pass
 
         
 
-"""if __name__ == "__main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Count the characters in a text file")
-    parser.add_argument("file")
+    parser.add_argument("-file")
+    parser.add_argument("--histogram", action="store_true",
+        help="plot a histogram of the character frequencies")
     args = parser.parse_args()
-    count_characters(args.file)"""
+    count_characters(args.file, args.histogram)
