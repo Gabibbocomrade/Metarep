e228ed0529ec619324e65a3fa8fe9e532ddfce2e
Gabibbocomrade <Tank2001@hotmail.it>
2024-09-30 18:56:34 +0200

    Concluso

--- Assignment_2.py
+++ Assignment_2.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import InterpolatedUnivariateSpline
 
 
-class ProbabilityDensityDistribution:
+class ProbabilityDensityDistribution(InterpolatedUnivariateSpline):
 
     def __init__(self, x, y):
+        spline = InterpolatedUnivariateSpline(x, y)
+        norm = spline.integral(x.min(), x.max())
         self._x = x
-        self._y = y
-        self._spline = InterpolatedUnivariateSpline(x, y)
+        self._y = y / norm
+        super().__init__(self._x, self._y)
         
     def evaluate(self, x):
         return self._spline(x)
@@ -16,10 +18,7 @@ class ProbabilityDensityDistribution:
     def plot(self):
         plt.plot(self._x, self._y, "o")
         x = np.linspace(self._x.min(), self._x.max(), 250)
-        plt.plot(x, self._spline(x))
-
-    def integral(self, x1, x2):
-        return self._spline.integral(x1, x2)
+        plt.plot(x, self(x))
     
     def normalization(self):
         return self.integral(self._x.min(), self._x.max())
@@ -30,6 +29,6 @@ if __name__=="__main__":
     y = np.exp(x)
     pdf = ProbabilityDensityDistribution(x, y)
     x0 = 0.5
-    print(pdf.integral(0., 1.))
+    print(pdf.normalization())
     pdf.plot()
     plt.show()
